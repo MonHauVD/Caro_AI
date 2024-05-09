30331879fbf06ed0362812f529520f3beff6a286
Nguyen Tung Duong <sieunhan283@gmail.com>
2024-05-09 16:38:46 +0700

    adding definitions

--- agent.py
+++ agent.py
@@ -1,5 +1,13 @@
 class Agent:
-    pass
+
+    def __init__(self) -> None:
+        pass
+
+    def get_move():
+        pass
+
+    def minimax():
+        pass
 
 if __name__ == '__main__':
     agent = Agent()
