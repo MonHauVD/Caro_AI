b7e0fbef2b5b44a54436e7e3f15bf9c995807194
Nguyen Tung Duong <sieunhan283@gmail.com>
2024-05-11 23:27:47 +0700

    Complete minimax

--- agent.py
+++ agent.py
@@ -6,7 +6,7 @@ THREE = 1000
 THREE_OBSTACLE = 500
 FOUR = 10000000
 FOUR_OBSTACLE = 2000
-WINNING = 500000000
+WINNING = 2000000000
 
 TWO_OPPONENT = -20
 TWO_OBSTACLE_OPPONENT = -3
@@ -21,29 +21,142 @@ class Agent:
 
     def __init__(self, max_depth: int) -> None:
         self.max_depth = max_depth
-        pass
 
-    def get_heuristic(self, grid: list[list[str]], current: str) -> int:
+    def compute(sequences: list[list[str]], current: str) -> int:
+        '''
+            Parameters
+            ----------------
+            sequences: consecutive cells from the board (rows, columns or diagonals)
+
+            current: 'X' or 'O', depend on current player move
+
+            Return
+            ---------------- 
+            Heuristic with the given sequences
+
+        '''
+
+        result = 0
+
+        player_count = 0
+        opponent_count = 0
+        has_obstacle = 1
+        for sequence in sequences:
+            for c in sequence:
+                if c == current:
+                    has_obstacle = 1
+                    player_count += 1
+                    # Handle opponent
+                    if opponent_count != 0:
+
+                        if opponent_count == 2:
+                            if has_obstacle:
+                                result += TWO_OBSTACLE_OPPONENT
+                            else:
+                                result += TWO_OPPONENT
+
+                        elif opponent_count == 3:
+                            if has_obstacle:
+                                result += THREE_OBSTACLE_OPPONENT
+                            else:
+                                result += THREE_OPPONENT
+
+                        elif opponent_count == 4:
+                            if has_obstacle:
+                                result += FOUR_OBSTACLE_OPPONENT
+                            else:
+                                result += FOUR_OPPONENT
+
+                        elif opponent_count == 5:
+                            result += LOSING
+
+                        opponent_count = 0
+                elif c != '.':
+                    has_obstacle = 1
+                    opponent_count += 1
+                    # Handle player
+
+                    if player_count != 0:
+                        if player_count == 2:
+                            if has_obstacle:
+                                result += TWO_OBSTACLE
+                            else:
+                                result += TWO
+
+                        elif player_count == 3:
+                            if has_obstacle:
+                                result += THREE_OBSTACLE
+                            else:
+                                result += THREE
+
+                        elif player_count == 4:
+                            if has_obstacle:
+                                result += FOUR_OBSTACLE
+                            else:
+                                result += FOUR
+
+                        elif player_count == 5:
+                            result += WINNING
+
+                else:
+                    player_count = 0
+                    opponent_count = 0
+                    has_obstacle = 0
+
+        if player_count == 2:
+            result += TWO_OBSTACLE
+        elif player_count == 3:
+            result += THREE_OBSTACLE
+        elif player_count == 4:
+            result += FOUR_OBSTACLE
+        elif player_count == 5:
+            result += WINNING
+
+        if opponent_count == 2:
+            result += TWO_OBSTACLE_OPPONENT
+        elif opponent_count == 3:
+            result += THREE_OBSTACLE_OPPONENT
+        elif opponent_count == 4:
+            result += FOUR_OBSTACLE_OPPONENT
+        elif opponent_count == 5:
+            result += LOSING
+
+        return result
+
+    def get_heuristic(self, game: Caro) -> int:
         '''
             Parameters
             ----------
-            grid: The XO game board 
 
-            current: 'X' or 'O', depend on which character player is playing with
+            game: Caro object, represent current game state
 
             Return
             --------------
             The heuristic corresponding to the current board and current player.
         '''
 
-        pass
+        current = game.XO
+        return self.compute(game.get_all_rows(), current) + self.compute(game.get_all_diagonals(), current) + self.compute(game.get_all_colummns, current)
 
-    def get_move():
-        pass
-
-    def minimax(self, game: Caro, depth: int, alpha: int, beta: int, maximizing_player: int = 1) -> list[list[int]]:
+    def get_move(self, game: Caro) -> list[list[int]]:
         '''
-            Implementing minimax algorithm for agent
+            Parameters
+            ----------
+
+            game: Caro object, represent current game state
+
+            Return
+            --------------
+            The best move of the current position
+        '''
+
+        best_score, best_move = self.minimax(
+            game, self.max_depth, -99999999999999, 99999999999999)
+        return best_move
+
+    def minimax(self, game: Caro, depth: int, alpha: int, beta: int, maximizing_player: int = 1) -> tuple[int, list[int]]:
+        '''
+            Implement the Minimax algorithm.
 
             Parameters
             ------------
@@ -59,16 +172,18 @@ class Agent:
 
             Return
             ------------
-            The heuristic for current node of the minimax tree.
+            The score of the best move and the best move coordinate.
 
         '''
+
         if depth == 0 or game.get_winner() != -1:
-            return self.get_heuristic(game.grid, game.XO)
+            return self.get_heuristic(game), None
 
         possible_moves = game.get_possible_moves()
 
         if maximizing_player:
             max_eval = -99999999999999
+            best_move = None
 
             for possible_move in possible_moves:
                 x = possible_move[0]
@@ -80,12 +195,16 @@ class Agent:
                 eval = self.minimax(new_game, depth - 1,
                                     alpha, beta, maximizing_player ^ 1)
                 max_eval = max(max_eval, eval)
+                if eval > max_eval:
+                    max_eval = eval
+                    best_move = [x, y]
                 alpha = max(alpha, eval)
                 if beta <= alpha:
                     break
-            return max_eval
+            return max_eval, best_move
         else:
             min_eval = 99999999999999
+            best_move = None
 
             for possible_move in possible_moves:
                 x = possible_move[0]
@@ -96,12 +215,15 @@ class Agent:
 
                 eval = self.minimax(new_game, depth - 1,
                                     alpha, beta, maximizing_player ^ 1)
-                min_eval = min(min_eval, beta)
+                if eval < min_eval:
+                    min_eval = eval
+                    best_move = [x, y]
+
                 beta = min(beta, eval)
                 if beta <= alpha:
                     break
-            return min_eval
+            return min_eval, best_move
 
 
 if __name__ == '__main__':
-    agent = Agent()
+    agent = Agent(max_depth=2)
