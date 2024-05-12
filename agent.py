63f0f7a346bcfda64b06bb3eb6542e02644031b2
Nguyen Tung Duong <sieunhan283@gmail.com>
2024-05-12 21:10:21 +0700

    agent fix

--- agent.py
+++ agent.py
@@ -1,4 +1,5 @@
 from caro import Caro
+import copy
 
 TWO = 10
 TWO_OBSTACLE = 5
@@ -19,10 +20,11 @@ LOSING = -1000000000
 
 class Agent:
 
-    def __init__(self, max_depth: int) -> None:
+    def __init__(self, max_depth: int, XO: str) -> None:
         self.max_depth = max_depth
+        self.XO = XO
 
-    def compute(sequences: list[list[str]], current: str) -> int:
+    def compute(self, sequences: list[list[str]]) -> int:
         '''
             Parameters
             ----------------
@@ -36,90 +38,107 @@ class Agent:
 
         '''
 
+        player = 0
+        opponent = 0
+        obstacle = 1
+
         result = 0
 
-        player_count = 0
-        opponent_count = 0
-        has_obstacle = 1
         for sequence in sequences:
             for c in sequence:
-                if c == current:
-                    has_obstacle = 1
-                    player_count += 1
-                    # Handle opponent
-                    if opponent_count != 0:
+                if c == self.XO:
+                    player += 1
+                    obstacle = 1
 
-                        if opponent_count == 2:
-                            if has_obstacle:
-                                result += TWO_OBSTACLE_OPPONENT
-                            else:
-                                result += TWO_OPPONENT
-
-                        elif opponent_count == 3:
-                            if has_obstacle:
-                                result += THREE_OBSTACLE_OPPONENT
-                            else:
-                                result += THREE_OPPONENT
-
-                        elif opponent_count == 4:
-                            if has_obstacle:
-                                result += FOUR_OBSTACLE_OPPONENT
-                            else:
-                                result += FOUR_OPPONENT
-
-                        elif opponent_count == 5:
+                    if opponent != 0:
+                        if opponent == 2 and obstacle == 0:
+                            result += TWO_OBSTACLE_OPPONENT
+                        elif opponent == 3 and obstacle == 0:
+                            result += THREE_OBSTACLE_OPPONENT
+                        elif opponent == 4 and obstacle == 0:
+                            result += FOUR_OBSTACLE_OPPONENT
+                        elif opponent == 5:
                             result += LOSING
 
-                        opponent_count = 0
-                elif c != '.':
-                    has_obstacle = 1
-                    opponent_count += 1
-                    # Handle player
+                    opponent = 0
 
-                    if player_count != 0:
-                        if player_count == 2:
-                            if has_obstacle:
+                elif c != '.':
+                    opponent += 1
+                    obstacle = 1
+
+                    if player != 0:
+                        if player == 2 and obstacle == 0:
+                            result += TWO_OBSTACLE
+                        elif player == 3 and obstacle == 0:
+                            result += THREE_OBSTACLE
+                        elif player == 4 and obstacle == 0:
+                            result += FOUR_OBSTACLE
+                        elif player == 5:
+                            result += WINNING
+                        player = 0
+
+                else:
+                    obstacle = 0
+                    if player != 0:
+                        if player == 2:
+                            if obstacle == 1:
                                 result += TWO_OBSTACLE
                             else:
                                 result += TWO
-
-                        elif player_count == 3:
-                            if has_obstacle:
+                        elif player == 3:
+                            if obstacle == 1:
                                 result += THREE_OBSTACLE
                             else:
                                 result += THREE
-
-                        elif player_count == 4:
-                            if has_obstacle:
+                        elif player == 4:
+                            if obstacle == 1:
                                 result += FOUR_OBSTACLE
                             else:
                                 result += FOUR
-
-                        elif player_count == 5:
+                        elif player == 5:
                             result += WINNING
+                    player = 0
 
-                else:
-                    player_count = 0
-                    opponent_count = 0
-                    has_obstacle = 0
+                    if opponent != 0:
+                        if opponent == 2:
+                            if obstacle == 1:
+                                result += TWO_OBSTACLE_OPPONENT
+                            else:
+                                result += TWO_OPPONENT
+                        elif opponent == 3:
+                            if obstacle == 1:
+                                result += THREE_OBSTACLE_OPPONENT
+                            else:
+                                result += THREE_OPPONENT
+                        elif opponent == 4:
+                            if obstacle == 1:
+                                result += FOUR_OBSTACLE_OPPONENT
+                            else:
+                                result += FOUR_OPPONENT
+                        elif opponent == 5:
+                            result += LOSING
 
-        if player_count == 2:
-            result += TWO_OBSTACLE
-        elif player_count == 3:
-            result += THREE_OBSTACLE
-        elif player_count == 4:
-            result += FOUR_OBSTACLE
-        elif player_count == 5:
-            result += WINNING
+                        opponent = 0
 
-        if opponent_count == 2:
-            result += TWO_OBSTACLE_OPPONENT
-        elif opponent_count == 3:
-            result += THREE_OBSTACLE_OPPONENT
-        elif opponent_count == 4:
-            result += FOUR_OBSTACLE_OPPONENT
-        elif opponent_count == 5:
-            result += LOSING
+        if opponent != 0:
+            if opponent == 2 and obstacle == 0:
+                result += TWO_OBSTACLE_OPPONENT
+            elif opponent == 3 and obstacle == 0:
+                result += THREE_OBSTACLE_OPPONENT
+            elif opponent == 4 and obstacle == 0:
+                result += FOUR_OBSTACLE_OPPONENT
+            elif opponent == 5:
+                result += LOSING
+
+        if player != 0:
+            if player == 2 and obstacle == 0:
+                result += TWO_OBSTACLE
+            elif player == 3 and obstacle == 0:
+                result += THREE_OBSTACLE
+            elif player == 4 and obstacle == 0:
+                result += FOUR_OBSTACLE
+            elif player == 5:
+                result += WINNING
 
         return result
 
@@ -135,8 +154,7 @@ class Agent:
             The heuristic corresponding to the current board and current player.
         '''
 
-        current = game.XO
-        return self.compute(game.get_all_rows(), current) + self.compute(game.get_all_diagonals(), current) + self.compute(game.get_all_colummns, current)
+        return self.compute(game.get_all_rows()) + self.compute(game.get_all_diagonals()) + self.compute(game.get_all_colummns)
 
     def get_move(self, game: Caro) -> list[list[int]]:
         '''
@@ -151,7 +169,7 @@ class Agent:
         '''
 
         best_score, best_move = self.minimax(
-            game, self.max_depth, -99999999999999, 99999999999999)
+            game, self.max_depth, -999999999999999, 999999999999999)
         return best_move
 
     def minimax(self, game: Caro, depth: int, alpha: int, beta: int, maximizing_player: int = 1) -> tuple[int, list[int]]:
@@ -183,13 +201,13 @@ class Agent:
 
         if maximizing_player:
             max_eval = -99999999999999
-            best_move = None
+            best_move = possible_moves[0]
 
             for possible_move in possible_moves:
                 x = possible_move[0]
                 y = possible_move[1]
 
-                new_game = game
+                new_game = copy.deepcopy(game)
                 new_game.make_move(x, y)
 
                 eval = self.minimax(new_game, depth - 1,
@@ -204,13 +222,13 @@ class Agent:
             return max_eval, best_move
         else:
             min_eval = 99999999999999
-            best_move = None
+            best_move = possible_moves[0]
 
             for possible_move in possible_moves:
                 x = possible_move[0]
                 y = possible_move[1]
 
-                new_game = game
+                new_game = copy.deepcopy(game)
                 new_game.make_move(x, y)
 
                 eval = self.minimax(new_game, depth - 1,
